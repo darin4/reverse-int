02048cd533745607faaff5593612008e8d93f799
darin4 <zvetochek23@mail.ru>
2024-08-05 18:47:35 +0300

    docs: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    let reversed = n.toString().split('').reverse().join('');
+     return (reversed);
+   }
